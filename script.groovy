e3d01a6e87e98a4a31ff75569e94fae20f4151b5
Tsemb <Tsembenhoi@gmail.com>
2024-12-17 00:40:36 +0200

    Add Groovy script

--- script.groovy
+++ script.groovy
@@ -10,21 +10,30 @@ def testApp() {
     sh "npm run test"
 }
 def buildAndPushImage() {
-    withCredentials([usernamePassword(credentialsId: 'docker-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')])
+    withCredentials([usernamePassword(credentialsId: 'docker-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
         script {
-           sh "docker build -t dancedevops/my-node-app:${IMAGE_NAME} ."
-           sh 'echo $PASS | docker login -u $USER --password-stdin'
-           sh "docker push dancedevops/my-node-app:${IMAGE_NAME}"
+            sh "docker build -t dancedevops/my-node-app:${IMAGE_NAME} -f Dockerfile ."
+            sh 'echo $PASS | docker login -u $USER --password-stdin'
+            sh "docker push dancedevops/my-node-app:${IMAGE_NAME}"
         }
+    }
 }
+
 def commitVersionUpdate() {
     withCredentials([usernamePassword(credentialsId: 'github-credentials', usernameVariable: 'USER', passwordVariable: 'TOKEN')]) {
-        sh 'git config --global user.email "tsembenhoi@gmail.com"'
-        sh 'git config --global user.name "TsembA"'
-        sh 'git remote set-url origin https://${USER}:${TOKEN}@github.com/TsembA/exercises-8-Build-Automation-CI-CD-with-Jenkins.git'
-        sh 'git add .'
-        sh 'git commit -m "ci: version bump" || echo "No changes to commit."'
-        sh 'git push origin HEAD:jenkins-jobs'
+        script {
+            sh 'git config --global user.email "tsembenhoi@gmail.com"'
+            sh 'git config --global user.name "TsembA"'
+            sh 'git remote set-url origin https://${USER}:${TOKEN}@github.com/TsembA/exercises-8-Build-Automation-CI-CD-with-Jenkins.git'
+            sh '''
+            if ! git diff --cached --exit-code > /dev/null; then
+                git commit -m "ci: version bump"
+                git push origin HEAD:jenkins-jobs
+            else
+                echo "No changes to commit."
+            fi
+            '''
+        }
     }
 }
 return this
